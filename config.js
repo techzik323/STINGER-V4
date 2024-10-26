b5f29a927da0d0156c30b315f2c228ceefff8c55
paskito002 <paschalmedia9@gmail.com>
2024-10-26 08:10:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ require("./database/module")
 
 //GLOBAL PAYMENT
 global.storename = "STINGER-V4"
-global.dana = "2348187637779"
+global.dana = "2349037524605"
 global.qris = "https://i.ibb.co/BnYRXcw/STINGER-V4.jpg"
 
 
 // GLOBAL SETTING
-global.owner = "2348187637779"
+global.owner = "2349037524605"
 global.namabot = " S҉ T҉ I҉ N҉ G҉ E҉ R҉- V҉ 4҉"
-global.nomorbot = "2348187637779"
+global.nomorbot = "2349037524605"
 global.namaCreator = "ES TEAMS"
 global.linkyt = "https://youtube.com/@esteams"
 global.autoJoin = false
