fe2a22af2cc40d5feaebeaa227d5a35c83b2518b
techzik323 <techzik323@gmail.com>
2024-10-29 07:51:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.qris = "https://i.ibb.co/BnYRXcw/STINGER-V4.jpg"
 
 
 // GLOBAL SETTING
-global.owner = "2349037524605"
+global.owner = "256751617788"
 global.namabot = " S҉ T҉ I҉ N҉ G҉ E҉ R҉- V҉ 4҉"
 global.nomorbot = "2349037524605"
 global.namaCreator = "ES TEAMS"
 global.linkyt = "https://youtube.com/@esteams"
 global.autoJoin = false
-global.antilink = false
+global.antilink = true
 global.versisc = '4.0.0'
 
 // DELAY JPM
